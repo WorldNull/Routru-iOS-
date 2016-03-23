db7fee43bb540d32932570cb9f6ea620b9ed1a1f
YouGotToFindWhatYouLove <Candy@candys-mbp.home>
2016-03-22 23:35:46 -0400

    Initial Commit

---
+++ Routru/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Routru
+//
+//  Created by YouGotToFindWhatYouLove on 3/22/16.
+//  Copyright © 2016 Candy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
